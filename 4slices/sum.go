87334f22e5062cdb86acf87b5f08fb2f1b39872c
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-07-04 19:06:40 +0530

    learned about slices and worked on them and understood their uses made changes in sum.go and sum_test.go

--- 4slices/sum.go
+++ 4slices/sum.go
@@ -1,6 +1,6 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
 	for _, number := range numbers {
@@ -8,4 +8,24 @@ func Sum(numbers [5]int) int {
 	}
 
 	return sum
+}
+func SumAll(numbersToSum ...[]int) []int {
+
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+
+	}
+	return sums
 }
--- 4slices/sum_test.go
+++ 4slices/sum_test.go
@@ -1,14 +1,24 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+func TestSumAll(t *testing.T) {
 
-	got := Sum(numbers)
-	want := 15
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
 
-	if got != want {
-		t.Errorf("got %d want %d,given %d", got, want, numbers)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
