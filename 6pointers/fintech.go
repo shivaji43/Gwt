0806668774cc2823e09d7fb1b2111819827f4ac1
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-07-05 18:28:16 +0530

    pointers and errors and how to handle them using helper functions

---
+++ 6pointers/fintech.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d Bitcoin", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+	return nil
+}
---
+++ 6pointers/fintech_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	})
+
+}
