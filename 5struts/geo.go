c88c3f5058d588ff74655a19c708925500d6a6ef
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-07-05 08:36:06 +0530

    learned to use struts for an Area function

---
+++ 5struts/geo.go
@@ -0,0 +1,16 @@
+package main
+
+type Circle struct {
+	Radius float64
+}
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
---
+++ 5struts/geo_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestArea(t *testing.T) {
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := Area(rectangle)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+}
