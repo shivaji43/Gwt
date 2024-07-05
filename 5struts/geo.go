53bc878a749f6864e2bc9f4c9d9b85d19661c000
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-07-05 08:45:49 +0530

    used methods to calculate Area on types like rectangle and circle

--- 5struts/geo.go
+++ 5struts/geo.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type Circle struct {
 	Radius float64
 }
@@ -8,9 +10,9 @@ type Rectangle struct {
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
 }
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
