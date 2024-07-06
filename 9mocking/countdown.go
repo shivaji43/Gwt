6e02d7295254753478947c103ac83c0f409469b2
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-07-06 19:44:13 +0530

    Used mocking to test the pausing of the seconds between the 3 , 2 ,1 and GO! and also used to to set custom time for sleeper using mocking

--- 9mocking/countdown.go
+++ 9mocking/countdown.go
@@ -53,3 +53,33 @@ func main() {
 	// Countdown(os.Stdout, spySleeper)
 	// fmt.Printf("Sleep calls: %d\n", spySleeper.Calls)
 }
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
